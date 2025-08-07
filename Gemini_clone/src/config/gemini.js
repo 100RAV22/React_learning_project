e5f0139f3f40f081b6e902ba17c7d2ff50193f3a
100RAV22 <saurav.kumar310704@gmail.com>
2025-08-08 02:46:54 +0530

    quiz app

---
+++ Gemini_clone/src/config/gemini.js
@@ -0,0 +1,50 @@
+const API_KEY = "AIzaSyAC4w_C_l1T44Ypxqwsh9qHRlIk1mSCn0w";
+
+// To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
+
+import {
+  GoogleGenAI,
+} from '@google/genai';
+
+async function main() {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.API_KEY,
+  });
+  const tools = [
+    {
+      googleSearch: {
+      }
+    },
+  ];
+  const config = {
+    thinkingConfig: {
+      thinkingBudget: -1,
+    },
+    tools,
+  };
+  const model = 'gemini-2.5-pro';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: `INSERT_INPUT_HERE`,
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  let fileIndex = 0;
+  for await (const chunk of response) {
+    console.log(chunk.text);
+  }
+}
+
+export default main();
